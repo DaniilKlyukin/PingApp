9a6a7a102ad8acf151c692a4084dc7de1ae67b4c
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-06-10 10:44:23 +0400

    delete old project

--- PingApp/UserStatistics.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PingApp
-{
-    public class UserStatistics
-    {
-        public required string Address { get; init; }
-        public required string? Nickname { get; init; }
-        public required List<WorkStatus> Statuses { get; init; }
-    }
-}

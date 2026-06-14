66f583319819ea282ed57f7ed7f74ceaba79a2d9
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-06-14 13:02:36 +0400

    refactor

--- PingApp.Application/Interfaces/IUserContext.cs
+++ PingApp.Application/Interfaces/IUserContext.cs
@@ -1,11 +1,12 @@
 ﻿using PingApp.Domain.Aggregates.UserAggregate.Common;
+using PingApp.Domain.Aggregates.UserAggregate.ValueObjects;
 
 namespace PingApp.Application.Interfaces;
 
 public interface IUserContext
 {
     UserId UserId { get; set; }
-    string? Username { get; set; }
+    Username? Username { get; set; }
     bool IsAdmin { get; set; }
     bool IsGuest { get; set; }
     bool IsAuthenticated { get; }
